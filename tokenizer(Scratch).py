68acdc2a173617584082e6c94107d923c35ea189
Satyam018 <satyam98358@gmail.com>
2025-05-22 14:21:21 +1000

    building tokenizer from scratch

---
+++ tokenizer(Scratch).py
@@ -0,0 +1,17 @@
+class Tokenizer:
+
+    def __init__(self):
+        self.vocab = {}
+
+    def build_vocab(self, text):
+        pass
+
+    def encode(self, text):
+        pass
+
+    def decode(self, token):
+        pass
+
+
+if __name__ == "__main__":
+    print("Satyam")
